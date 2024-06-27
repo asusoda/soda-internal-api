32bebb9cab8c449be72dcddd0255bcf2fe8c02cb
code-wolf-byte <hackerstark37@gmail.com>
2024-06-27 23:11:22 +0530

    rename

--- server/main.py
+++ server/main.py
@@ -1,5 +1,5 @@
 from shared import app
-from modules.bot.bot import game_blueprint
+from server.modules.bot.api import game_blueprint
 
 app.register_blueprint(game_blueprint, url_prefix="/game")
 if __name__ == "__main__":
