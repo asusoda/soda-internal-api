388936597ece8a2cd42dd98bbfb05cf02440257c
code-wolf-byte <hackerstark37@gmail.com>
2024-09-01 17:23:04 -0700

    Update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
-#!bin/bash
+#!/bin/bash
 
 echo "Opening the API directory"
-cd /root/soda-internal-api
+cd /root/soda-internal-api || { echo "Failed to change directory. Exiting."; exit 1; }
 
 echo "Stashing any local changes"
 git stash
@@ -9,4 +9,10 @@ git stash
 echo "Pulling the latest changes from the repository"
 git pull
 
+echo "Checking out the main branch"
 git checkout main
+
+echo "Restarting the daemon process for soda-api.service"
+sudo systemctl restart soda-api.service
+
+echo "Daemon process restarted successfully."
