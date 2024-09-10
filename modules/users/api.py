8d3c0bc545ed84a0d00fc9110ca4b7e7676f9f9f
code-wolf-byte <hackerstark37@gmail.com>
2024-09-09 22:58:12 -0700

    Removed unused imports

--- modules/users/api.py
+++ modules/users/api.py
@@ -1,8 +1,3 @@
-from selenium import webdriver
-from selenium.webdriver import ChromeOptions
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import logging
 import sys
 import time
