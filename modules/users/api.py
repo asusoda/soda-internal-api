4f84db3e7b7dcf1a64057e968614d1d17d9afc12
code-wolf-byte <hackerstark37@gmail.com>
2024-09-09 22:57:46 -0700

    Removed Invite script

--- modules/users/api.py
+++ modules/users/api.py
@@ -11,7 +11,6 @@ import os
 from flask import Blueprint, jsonify, request
 from modules.auth.decoraters import auth_required, error_handler
 from modules.points.models import User
-from modules.users.invite import InvitationSender
 from shared import config, db_connect
 
 # Flask Blueprint for users
@@ -52,30 +51,3 @@ def create_user():
     add_user_to_db(db_connect, asu_id, name, email, standing, major)
     return jsonify({"message": "User created successfully"}), 201
 
-'''
-Deprecatation warning: This function has been deprecated as selenium
-is not working properly
-'''
-@users_blueprint.route("/invite", methods=["POST"])
-def invite_users():
-    email = request.args.get("email")
-    if email:
-        pattern = r"^[a-zA-Z0-9._%+-]+@asu\.edu$"
-        try:
-            invitation_sender = InvitationSender(config.USERNAME, config.PASSWORD)
-            if re.match(pattern, email):
-                invitation_sender.emails.add(email)
-                invitation_sender.add_emails_and_send_invitations()
-                return jsonify({"message": "Email added"}), 200
-            else:
-                return jsonify({"error": "Invalid email format"}), 400
-
-        except Exception as e:
-            logging.error(f"An error occurred while inviting users: {e}")
-            return jsonify({"error": "Failed to send invitation"}), 500
-
-        finally:
-            # Ensure the WebDriver is closed after processing the request
-            invitation_sender.close()
-    else:
-        return jsonify({"error": "No email provided"}), 400
