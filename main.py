a6c341af70b64aef987a6345de68f8d91db947a2
Ben Juntilla <ben@benjuntilla.com>
2025-03-22 22:34:13 -0700

    prevent threading issues

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 from flask import Flask
-from shared import app
+from shared import app, bot
 from modules.public.api import public_blueprint
 from modules.points.api import points_blueprint
 from modules.users.api import users_blueprint
 from modules.utils.db import DBConnect
 from modules.auth.api import auth_blueprint
 from modules.calendar.api import calendar_blueprint
-
-
-
-# Initialize and create tables
-db_connect = DBConnect()
-db_connect.check_and_create_tables()
+import threading
 
 # Register Blueprints
 app.register_blueprint(public_blueprint, url_prefix="/")
@@ -21,4 +16,10 @@ app.register_blueprint(auth_blueprint, url_prefix="/auth")
 app.register_blueprint(calendar_blueprint, url_prefix="/calendar")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Start Discord bot in a separate thread
+    bot_thread = threading.Thread(target=bot.run)
+    bot_thread.daemon = True
+    bot_thread.start()
+    
+    # Start Flask app
+    app.run(host='0.0.0.0', port=8000, debug=True)
